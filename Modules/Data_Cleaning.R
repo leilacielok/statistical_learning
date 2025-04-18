ba9d006990b5eb88404af11bdead7fda82bb711e
Leila Cielok <leila.cielok@studenti.unimi.it>
2025-04-18 14:49:33 +0200

    Fix linkage between files

--- Modules/Data_Cleaning.R
+++ Modules/Data_Cleaning.R
@@ -1,6 +1,3 @@
-library(ggplot2)
-library(gplots)
-library(ggrepel)
 library(plotly)
 library(dplyr)
 library(e1071)
@@ -9,7 +6,6 @@ if (!exists("plot_graphs")) plot_graphs <- FALSE
 
 # 1. Import the dataset
 life_expectancy_dataset <- read.csv("life.expectancy.csv")
-str(life_expectancy_dataset)
 
 # 2. Fix character variables
 life_expectancy_dataset <- life_expectancy_dataset %>%
@@ -18,32 +14,24 @@ life_expectancy_dataset <- life_expectancy_dataset %>%
             ~ as.numeric(gsub(",", "", .)))
 
 # 3. Substitute NA with variables' means
-life_expectancy_dataset <- data.frame(lapply(life_expectancy_dataset, function(x) ifelse(is.na(x), mean(x, na.rm = TRUE), x)))
+life_expectancy_dataset <- data.frame(lapply(
+  life_expectancy_dataset, 
+  function(x) ifelse(is.na(x), mean(x, na.rm = TRUE), x)
+  ))
 
-# 4. Check normality
-qqnorm(life_expectancy_dataset$life_expectancy)
-qqline(life_expectancy_dataset$life_expectancy, col="blue")
-shapiro.test(life_expectancy_dataset$life_expectancy)
-
-# 5. Variables' distributions
-par(mfrow=c(4,5))  
-for(i in 3:20) {
-  hist(life_expectancy_dataset[, i], main=names(life_expectancy_dataset)[i], col="lightblue")
-}
-par(mfrow=c(1,1))  
-
-# 6. Logarithmic transformation of skewed variables 
+# 4. Logarithmic transformation of skewed variables 
 skew_vals <- sapply(life_expectancy_dataset[3:20], skewness)
-print(skew_vals)
 skewed_pos <- names(skew_vals[skew_vals > 1])
 skewed_neg <- names(skew_vals[skew_vals < -1])
+
 for (var in skewed_pos) {
-  life_expectancy_dataset[[var]] <- log1p(life_expectancy_dataset[[var]])} 
+  life_expectancy_dataset[[var]] <- log1p(life_expectancy_dataset[[var]])
+} 
 for (var in skewed_neg) {
   life_expectancy_dataset[[var]] <- log1p(max(life_expectancy_dataset[[var]], na.rm = TRUE) + 1 - life_expectancy_dataset[[var]])
 }
 
-# 7. Standardization of the data
+# 5. Standardization of the data
 num_vars <- life_expectancy_dataset[, sapply(life_expectancy_dataset, is.numeric)]
 num_vars_scaled <- scale(num_vars)
 
@@ -52,24 +40,11 @@ scaled_lifeexp <- cbind(
   as.data.frame(num_vars_scaled)
 )
 
-# 8. Encode Status in a dummy
+# 6. Encode Status in a dummy
 scaled_lifeexp$Status <- ifelse(life_expectancy_dataset$Status == "Developed", 1, 0)
 
-# 9. Handle multicollinearity: drop highly correlated variables
+# 7. Handle multicollinearity: drop highly correlated variables
 corr_matrix <- cor(scaled_lifeexp[, -1])
-heatmap.2(corr_matrix, 
-          main="Correlation map", 
-          cex.main=0.8, 
-          cexRow=0.6, 
-          cexCol=0.6, 
-          lwd=0.5,   
-          trace="none", 
-          sepwidth=c(0.01, 0.01), 
-          colsep=1:ncol(corr_matrix), 
-          rowsep=1:nrow(corr_matrix),
-          srtCol=45,
-          srtRow=45)
-
 high_corr_pairs <- which(abs(corr_matrix) >= 0.9 & abs(corr_matrix) < 1, arr.ind = TRUE)
 high_corr_df <- data.frame(
   Var1 = rownames(corr_matrix)[high_corr_pairs[, 1]],
@@ -79,15 +54,45 @@ high_corr_df <- data.frame(
 high_corr_df <- high_corr_df[high_corr_df$Var1 < high_corr_df$Var2, ]
 
 vars_to_drop <- c("infant_deaths", "diphtheria", "thinness5_9years", "inc_composition")
-scaled_lifeexp <- scaled_lifeexp[, !(names(scaled_lifeexp) %in% vars_to_drop)]
+scaled_lifeexp_final <- scaled_lifeexp[, !(names(scaled_lifeexp) %in% vars_to_drop)]
 
-# 10. Return only useful objects
+# 8. Graphs: do not print them when the module is called
+if (plot_graphs) {
+  library(gplots)
+  
+  # Check normality: QQ plot and normality test
+  qqnorm(life_expectancy_dataset$life_expectancy)
+  qqline(life_expectancy_dataset$life_expectancy, col="blue")
+  print(shapiro.test(life_expectancy_dataset$life_expectancy))
+  
+  # Histograms for variables' distributions
+  par(mfrow=c(4,5))  
+  for(i in 3:20) {
+    hist(life_expectancy_dataset[, i], main=names(life_expectancy_dataset)[i], col="lightblue")
+  }
+  par(mfrow=c(1,1))
+  
+  # Correlation map
+  heatmap.2(corr_matrix, 
+            main="Correlation map", 
+            cex.main=0.8, 
+            cexRow=0.6, 
+            cexCol=0.6, 
+            lwd=0.5,   
+            trace="none", 
+            sepwidth=c(0.01, 0.01), 
+            colsep=1:ncol(corr_matrix), 
+            rowsep=1:nrow(corr_matrix),
+            srtCol=45,
+            srtRow=45)
+}
+
+# 9. Return only useful objects
 result <- list(
-  cleaned_data = scaled_lifeexp,
+  cleaned_data = scaled_lifeexp_final,
   original_data = life_expectancy_dataset,
   dropped_variables = vars_to_drop,
-  correlation_matrix = corr_matrix,
-  high_corr_table = high_corr_df
+  correlation_matrix = corr_matrix
 )
 
 return(result)
